50118b79daf5e65090304d110c12620644c19310
Rohitsarswat <83912817+Rohitsarswat@users.noreply.github.com>
2021-10-30 17:55:53 +0530

    Update CannonBall.js

--- js/CannonBall.js
+++ js/CannonBall.js
@@ -5,6 +5,7 @@ class CannonBall {
       isStatic: true
     };
     this.r = 30;
+    this.trajectory = [];
     this.body = Bodies.circle(x, y, this.r, options);
     this.image = loadImage("./assets/cannonball.png");
     World.add(world, this.body);
@@ -22,6 +23,13 @@ class CannonBall {
 
   display() 
   {
+    if(this.body.velocity.x > 0 && this.body.position.x > 200){
+      var position = [this.body.position.x, this.body.position.y]
+      this.trajectory.push(position)      
+    }
+    for(var i = 0; i < this.trajectory.length; i++){
+      image(this.image, this.trajectory[i][0], this.trajectory[i][1], 5, 5);
+    }
     var pos = this.body.position;
     push();
     imageMode(CENTER);
